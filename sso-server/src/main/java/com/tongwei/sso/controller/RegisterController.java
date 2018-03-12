5fc6aaff601b313be2fc11ce78912658f5cea673
yzgod <919573416@qq.com>
2018-03-12 10:54:24 +0800

    注册

--- sso-server/src/main/java/com/tongwei/sso/controller/RegisterController.java
+++ sso-server/src/main/java/com/tongwei/sso/controller/RegisterController.java
@@ -1,5 +1,6 @@
 package com.tongwei.sso.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.tongwei.common.BaseController;
@@ -11,11 +12,13 @@ import com.tongwei.common.util.ResultUtil;
  * @date 2018年3月1日 上午10:35:47
  * @description 用户注册
  */
+@Controller
 public class RegisterController extends BaseController {
 
 	// 用户注册
-    @PostMapping(value = "/register")
+    @PostMapping(value = "/register_test")
     public Result register() throws Exception {
+        //注册用户有自己的用户组
     	return ResultUtil.doFailure("按业务自行实现注册功能!");
     }
     
